a0877dcfabdb9d175039f7f5743c79efe3374b07
housseyn <attafi-housseyn@hotmail.fr>
2019-11-11 12:06:42 +0100

    challenge date

--- TestDate.java
+++ TestDate.java
@@ -16,8 +16,12 @@ class TestDate {
         System.out.println("Please fill a year :");
         int year = sc.nextInt();
         sc.close();
-
         // TODO : your code after this line
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar c = new GregorianCalendar(year,month-1,day);
+        Date d = c.getTime();
+        System.out.println("The date is : " +sdf.format(d));
+        System.out.println("The day of week is : " +c.get(Calendar.DAY_OF_WEEK));
         
     }
 }
